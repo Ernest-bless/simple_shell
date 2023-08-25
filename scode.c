ca5226b4c0bc3e7ec2cfc77145c0ed06383abed9
Ernest-bless <blessernesto21@gmail.com>
2023-08-25 13:59:57 -0700

    0x16. C - Simple Shell

---
+++ scode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("I am yet to be a software engineer.\n");
+	}
+	return (0);
+}
